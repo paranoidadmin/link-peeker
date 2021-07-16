df51a057ebd9e7c699521baabfa9c858e07bd4f2
thelandiman <notarealemail>
2021-07-16 13:31:52 -0400

    Add support for buff.ly

--- lp.py
+++ lp.py
@@ -12,7 +12,7 @@ import requests
 Configuration:
 """
 #Supported shortening services, stored in array.
-services = ["tinyurl", "bitly", "bit.ly"]
+services = ["tinyurl", "bitly", "bit.ly", "buff.ly"]
 
 
 """
